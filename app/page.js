7da9877ba251702f646ecc5f2080f466a9b1fd05
Kamil Uhryn <kamiluhryn@gmail.com>
2023-11-23 20:59:49 +0100

    Added link to login page

--- app/page.js
+++ app/page.js
@@ -2,12 +2,22 @@
 
 import Header from 'modules/photos/components/Header';
 import Uploader from 'modules/photos/components/Uploader';
+import { useRouter } from 'next/navigation';
 
 const Page = () => {
+  const router = useRouter();
+
   return (
-    <div className="w-full min-h-screen px-5 pt-12 pb-20 flex flex-col space-y-8 items-center justify-start">
+    <div className="w-full min-h-screen px-5 pt-8 pb-20 flex flex-col space-y-6 items-center justify-start">
       <Header showIntro />
       <Uploader intro />
+      <div className="flex flex-col space-y-2 items-center justify-center">
+        <div>Masz konto?</div>
+        <div>👇</div>
+        <button onClick={() => router.push('/login')} className="btn">
+          Zaloguj się
+        </button>
+      </div>
     </div>
   );
 };
